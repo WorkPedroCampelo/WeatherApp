95e7f03954ef2733471f04418641fb87be029025
Pedro Campelo Rico <work.pedrocampelo@gmail.com>
2023-08-24 17:28:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,19 @@ fetch(apiUrl)
 .then(response => response.json())
 
 .then(data => {
-    const wheatherDate= data.DailyForecasts[0].Date
+    let wheatherDate= data.DailyForecasts[0].Date
+    wheatherDate = wheatherDate.substring(0,10);
+    
+
     const textOfTheDay = data.DailyForecasts[0].Day.IconPhrase //Extrae el "icono" por lo que te dirá que tipo de día hace
     const temperatureMinF = data.DailyForecasts[0].Temperature.Minimum.Value;
     const temperatureMaxF = data.DailyForecasts[0].Temperature.Maximum.Value;
 
         // Convertir temperaturas de Fahrenheit a Celsius
-        const temperatureMinC = (temperatureMinF - 32) * 5/9;
-        const temperatureMaxC = (temperatureMaxF - 32) * 5/9;
+        let temperatureMinC = (temperatureMinF - 32) * 5/9;
+        let temperatureMaxC = (temperatureMaxF - 32) * 5/9;
+        temperatureMinC=temperatureMinC.toFixed(2);
+        temperatureMaxC=temperatureMaxC.toFixed(2);
 
 
     weatherInfo.innerHTML= `Date: ${wheatherDate}<br>
