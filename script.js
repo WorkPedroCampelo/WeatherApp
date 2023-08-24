24a91c8e23a3df6b4bc4411018c8718ced17c893
Pedro Campelo Rico <work.pedrocampelo@gmail.com>
2023-08-24 20:00:11 +0200

    PMV

--- script.js
+++ script.js
@@ -3,34 +3,50 @@ const apiKey = 'bCZgMSxcSgIIBXk5HleMwdgXefIQTrzN';
 const locationInput= document.getElementById('LocationInput');
 const weatherInfo = document.getElementById('weatherInfo');
 
-const apiUrl = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/307772?apikey=bCZgMSxcSgIIBXk5HleMwdgXefIQTrzN`;
+const apiUrl = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/307772?apikey=bCZgMSxcSgIIBXk5HleMwdgXefIQTrzN`;
 
 fetch(apiUrl)
 .then(response => response.json())
 
+fetch(apiUrl)
+.then(response => response.json())
 .then(data => {
-    let wheatherDate= data.DailyForecasts[0].Date
-    wheatherDate = wheatherDate.substring(0,10);
-    
+    const weatherInfoContainer = document.getElementById("weather-info-container"); // Supongo que tienes un elemento contenedor con este ID
 
-    const textOfTheDay = data.DailyForecasts[0].Day.IconPhrase //Extrae el "icono" por lo que te dirá que tipo de día hace
-    const temperatureMinF = data.DailyForecasts[0].Temperature.Minimum.Value;
-    const temperatureMaxF = data.DailyForecasts[0].Temperature.Maximum.Value;
+    data.DailyForecasts.forEach(forecast => {
+        let weatherDate = forecast.Date.substring(0, 10);
+        const textOfTheDay = forecast.Day.IconPhrase;
+        const temperatureMinF = forecast.Temperature.Minimum.Value;
+        const temperatureMaxF = forecast.Temperature.Maximum.Value;
 
-        // Convertir temperaturas de Fahrenheit a Celsius
-        let temperatureMinC = (temperatureMinF - 32) * 5/9;
-        let temperatureMaxC = (temperatureMaxF - 32) * 5/9;
-        temperatureMinC=temperatureMinC.toFixed(2);
-        temperatureMaxC=temperatureMaxC.toFixed(2);
+        let temperatureMinC = (temperatureMinF - 32) * 5 / 9;
+        let temperatureMaxC = (temperatureMaxF - 32) * 5 / 9;
+        temperatureMinC = temperatureMinC.toFixed(2);
+        temperatureMaxC = temperatureMaxC.toFixed(2);
 
+        let wheatherphoto = "sunny.png";
+        if (textOfTheDay=="Intermittent clouds") {
+          wheatherphoto="sunAndClouds.png";
+        }
 
-    weatherInfo.innerHTML= `<span>Date: ${wheatherDate}<br></span>
-                            <span>Summary: ${textOfTheDay}!! <br>   </span>
-                            <span>Min temp: ${temperatureMinC}°C<br></span>
-                            <span>Max temp: ${temperatureMaxC}°C<br></span>`;
+        const weatherInfo = document.createElement("div");
+        weatherInfo.innerHTML = ` <div class="mt-5 flex" style="background-color: rgba(33, 231, 195, 0.363)">
+                                    <div>
+                                    <img class="max-h-32" src="icons/${wheatherphoto}" alt="caca">
+                                    
+                                   </div>
+                                    <div class="pt-3">
+                                      <span>Date: ${weatherDate}<br></span>
+                                      <span>Summary: ${textOfTheDay}!! <br></span>
+                                      <span>Min temp: ${temperatureMinC}°C<br></span>
+                                      <span>Max temp: ${temperatureMaxC}°C<br></span>
+                                    </div>
 
+                                  </div>`;
+
+        weatherInfoContainer.appendChild(weatherInfo);
+    });
 })
-
     
 .catch(error => {
     console.error('Error al obtener los datos:', error);
